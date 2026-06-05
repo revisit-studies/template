de2603691f5f2b5d31cd4114500e8d28ecdc67c1
Jay Kim <76601570+yeonkim1213@users.noreply.github.com>
2026-06-04 23:03:57 -0600

    Fix crashing when there is only placeholder comment

--- src/public/tutorial/assets/replication/ScatterWrapper.tsx
+++ src/public/tutorial/assets/replication/ScatterWrapper.tsx
@@ -1,1 +1,10 @@
 // Placeholder for the ScatterWrapper component
+import { Text } from '@mantine/core';
+import { StimulusParams } from '../../../../store/types';
+
+export default function ScatterWrapper({ parameters }: StimulusParams<{ r1: number; r2: number }>) {
+
+    return (
+        <Text>Scatter Wrapper placeholder</Text>
+    );
+}
